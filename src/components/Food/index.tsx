e6d011056b7472a63c177779517c5972bb8a8c5c
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-07-10 13:43:52 -0300

    refactor class

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,42 +1,38 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
 import api from '../../services/api';
 
-class Food extends Component {
-  constructor(props) {
-    super(props);
+interface IFood {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  available: boolean;
+  image: string
+}
 
-    const { available } = this.props.food;
-    this.state = {
-      isAvailable: available
-    };
-  }
+interface IFoodProps {
+  food: IFood;
+  handleEditFood: (food: IFood) => void;
+  handleDelete: (id: number) => void;
+}
 
-  toggleAvailable = async () => {
-    const { food } = this.props;
-    const { isAvailable } = this.state;
+export function Food({food, handleEditFood, handleDelete}: IFoodProps) {
+  const [isAvailable, setIsAvailable] = useState(false)
+
+  async function toggleAvailable() {
 
     await api.put(`/foods/${food.id}`, {
       ...food,
       available: !isAvailable,
     });
 
-    this.setState({ isAvailable: !isAvailable });
+    setIsAvailable(!isAvailable);
   }
 
-  setEditingFood = () => {
-    const { food, handleEditFood } = this.props;
-
-    handleEditFood(food);
-  }
-
-  render() {
-    const { isAvailable } = this.state;
-    const { food, handleDelete } = this.props;
-
-    return (
+  return (
       <Container available={isAvailable}>
         <header>
           <img src={food.image} alt={food.name} />
@@ -53,7 +49,7 @@ class Food extends Component {
             <button
               type="button"
               className="icon"
-              onClick={this.setEditingFood}
+              onClick={() => handleEditFood(food)}
               data-testid={`edit-food-${food.id}`}
             >
               <FiEdit3 size={20} />
@@ -77,7 +73,7 @@ class Food extends Component {
                 id={`available-switch-${food.id}`}
                 type="checkbox"
                 checked={isAvailable}
-                onChange={this.toggleAvailable}
+                onChange={toggleAvailable}
                 data-testid={`change-status-food-${food.id}`}
               />
               <span className="slider" />
@@ -85,8 +81,8 @@ class Food extends Component {
           </div>
         </section>
       </Container>
-    );
-  }
-};
+  )
+}
+
 
 export default Food;
--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,30 +1,38 @@
-import { Component, createRef } from 'react';
+import { useRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
 
-class ModalAddFood extends Component {
-  constructor(props) {
-    super(props);
+interface IFood {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  available: boolean;
+  image: string
+}
 
-    this.formRef = createRef();
-  }
+interface IModalAddFood {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  editingFood: (id: number) => void;
+  handleAddFood: (data: IFood) => void;
+}
 
-  handleSubmit = async data => {
-    const { setIsOpen, handleAddFood } = this.props;
 
+export function ModalAddFood({isOpen, setIsOpen, handleAddFood}: IModalAddFood) {
+  const formRef = useRef<HTMLFormElement>(null)
+
+  async function handleSubmit(data: IFood) {
     handleAddFood(data);
     setIsOpen();
-  };
-
-  render() {
-    const { isOpen, setIsOpen } = this.props;
-
-    return (
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit}>
+  }
+  
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Novo Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -39,9 +47,8 @@ class ModalAddFood extends Component {
             </div>
           </button>
         </Form>
-      </Modal>
-    );
-  }
-};
+    </Modal>
+  )
+}
 
 export default ModalAddFood;
